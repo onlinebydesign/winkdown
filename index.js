9efd64e0ed887a052096da38181a211279d59930
Jedediah Smith <jed@obdstudios.com>
2014-11-07 14:35:59 -0700

    I downloaded all my images. Yes!!!!
    I still don't have folder names figured out.

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var async = require('async');
 var request = require('request');
+var fs = require('fs');
+var path = require('path');
+var url = require('url');
 var cookieJar = request.jar();
 //request = request.defaults({jar: cookieJar});
 var cheerio = require('cheerio');
@@ -8,35 +11,104 @@ var credentials = require('./credentials'); // Rename credentials.copy.js to cre
 
 var viewState = '';
 var eventValidation = '';
-var baseUrl =' http://www.winkflash.com/';
+var baseUrl = 'http://www.winkflash.com';
 
-var folderQueue = async.queue(function (href, callback) {
+var headers = {
+    'Cache-Control': 'max-age=0',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    'Origin': baseUrl,
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/37.0.2062.120 Chrome/37.0.2062.120 Safari/537.36',
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Referer': baseUrl + '/photo/signin.aspx',
+    'Accept-Encoding': 'gzip,deflate',
+    'Accept-Language': 'en-US,en;q=0.8'
+};
+
+var folderQueue = async.queue(function (folderHref, callback) {
     // Create a DOM object
-    var $ = cheerio.load(body);
-    $('a[id^=ctl00_ContentPlaceHolder1_ct]').each(function() {
-        //if (this.href.indexOf('folder') !== -1)
-            //add to folderQueue(this.href)
-        //else
-        //add to imageQueue(this.href)
-        //folderQueue.push(this.href.substr(this.href.indexOf('?')));
+    request({
+        method: 'GET',
+        uri: baseUrl + '/photo/' + folderHref,
+        jar: cookieJar,
+        headers: headers,
+        followAllRedirects: true
+    }, function (err, response, body) {
+        if (!err) {
+            // Create a DOM object
+            //console.log(body);
+            var $ = cheerio.load(body);
+            $('a[id^=ctl00_ContentPlaceHolder1_ct]').each(function (i, e) {
+                var href = e.attribs.href;
+                console.log(href);
+                if (href.indexOf('folder') !== -1) {
+                    folderQueue.push(href);
+                } else {
+                    fileQueue.push(href.substr(href.indexOf('?')).replace('ic', 'c').replace('m', 'i'));
+                }
+            });
+
+            callback(null);
+        } else {
+            callback(err);
+        }
     });
-    console.log('loading: ' + href);
-    callback();
 }, 2);
 
-var fileQueue = async.queue(function (task, callback) {
-    console.log('hello ' + task.name);
-    callback();
+var fileQueue = async.queue(function (fileHref, callback) {
+    var req = request({
+        method: 'GET',
+        uri: baseUrl + '/photo/imagew1.aspx' + fileHref,
+        jar: cookieJar,
+        headers: headers,
+        followAllRedirects: true
+    }).on('response', function (response, err) {
+        if (!err) {
+            // Create a DOM object
+            //console.log(body);
+            var filename,
+                contentDisp = response.headers['content-disposition'];
+            if (contentDisp && /^attachment/i.test(contentDisp)) {
+                filename = contentDisp.toLowerCase()
+                    .split('filename=')[1]
+                    .split(';')[0]
+                    .replace(/"/g, '');
+            } else {
+                console.log(response);
+                filename = path.basename(url.parse(baseUrl + '/photo/imagew1.aspx' + fileHref).path);
+            }
+            console.log(filename);
+            req.pipe(fs.createWriteStream(path.join(__dirname, filename)));
+
+            callback(null);
+        } else {
+            callback(err);
+        }
+    });
 }, 2);
 
+var splitCookies = function (headers) {
+    if (headers['set-cookie']) {
+        var setCookies = headers['set-cookie'];
+        for (var i = 0; i < setCookies.length; i++) {
+            var splitCookie = setCookies[i];
+            //for (var j = 0; j < splitCookie.length; j++) {
+            cookieJar.setCookie(request.cookie(splitCookie), baseUrl, function (err, cookie) {
+                if (err) {
+                    return console.error('signin failed:', err);
+                }
+            });
+
+            //}
+        }
+    }
+};
 
 async.series([
-    function(callback) {
+    function (callback) {
         // Get the viewState, eventValidation, and cookies
-        request(baseUrl + 'photo/signin.aspx', function(err, response, body) {
+        request(baseUrl + '/photo/signin.aspx', function (err, response, body) {
             if (!err) {
-                var cookieSessionId = request.cookie(response.headers['set-cookie'][0]);
-                cookieJar.setCookie(cookieSessionId, baseUrl);
+                splitCookies(response.headers);
                 var $ = cheerio.load(body);
                 // Get the viewState
                 viewState = $('#__VIEWSTATE').val();
@@ -48,35 +120,53 @@ async.series([
             }
         });
     },
-    function(callback) {
+    function (callback) {
         // Signin
         var formData = {
             __EVENTTARGET: '',
             __EVENTARGUMENT: '',
             __VIEWSTATE: viewState,
-            ctl00_ContentPlaceHolder1_siteid: 1,
-            ctl00_ContentPlaceHolder1_userid: credentials.username,
-            ctl00_ContentPlaceHolder1_password: credentials.password,
+            'ctl00$ContentPlaceHolder1$siteid': 1,
+            'ctl00$ContentPlaceHolder1$userid': credentials.username,
+            'ctl00$ContentPlaceHolder1$password': credentials.password,
             __EVENTVALIDATION: eventValidation,
-            ctl00_ContentPlaceHolder1_Button1: 'Log In'
+            'ctl00$ContentPlaceHolder1$Button1': 'Log+In'
         };
+        //console.log(formData);
 
-        request.post({url:baseUrl + 'photo/signin.aspx', formData: formData, jar: cookieJar}, function optionalCallback(err, httpResponse, body) {
+        request({
+            method: 'POST',
+            uri: baseUrl + '/photo/signin.aspx',
+            form: formData,
+            jar: cookieJar,
+            headers: headers,
+            followAllRedirects: true
+        }, function (err, httpResponse, body) {
             if (err) {
                 return console.error('signin failed:', err);
             }
-            console.log('Server responded with:', httpResponse, body);
+            splitCookies(httpResponse.headers);
+            //console.log('Server responded with:', body);
             callback(null);
         });
     },
-    function(callback) {
+    function (callback) {
         // Go to the home page and begin the recursive image scraping
-        request('http://www.winkflash.com/photo/myhome.aspx', function(err, response, body) {
+        request({
+            method: 'GET',
+            uri: baseUrl + '/photo/myhome.aspx',
+            jar: cookieJar,
+            headers: headers,
+            followAllRedirects: true
+        }, function (err, response, body) {
             if (!err) {
                 // Create a DOM object
+                //console.log(body);
                 var $ = cheerio.load(body);
-                $('a[id^=ctl00_ContentPlaceHolder1_ct]').each(function() {
-                    folderQueue.push(this.href.substr(this.href.indexOf('?')));
+                $('a[id^=ctl00_ContentPlaceHolder1_ct]').each(function (i, e) {
+                    var href = e.attribs.href;
+                    console.log(href);
+                    folderQueue.push(href);
                 });
 
                 callback(null);
@@ -84,4 +174,3 @@ async.series([
         });
     }
 ]);
-
